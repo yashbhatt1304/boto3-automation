0a337b80cd322a0a2343891bcef0adcd17b4a048
Yash_Bhatt <yashbhatt1304@gmail.com>
2024-07-17 01:45:39 +0530

    Adding index.html file and upload function for s3 bucket

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import boto3
 
 bucketName="yashbucket101"
+
+########## creating S3 storage for storing static files ##########
 def creates3storage():
     s3=boto3.client('s3')
     response = s3.create_bucket(
@@ -8,7 +10,12 @@ def creates3storage():
         CreateBucketConfiguration={'LocationConstraint': 'ap-northeast-2',},
     )
     return response["Location"]
-
 s3BucketUrl=creates3storage()
 
 
+###########  Uploading static file f=of our Application in S3 bucket #########
+def putDataInS3():
+    s3=boto3.client('s3')
+    s3.upload_file('./index.html', bucketName, 'index.html')
+
+putDataInS3()
